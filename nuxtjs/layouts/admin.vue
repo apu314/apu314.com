b2ddbc939f16d8ebc3ee370278f8b0933b5b8d09
apu314 <apu314@apu314.com>
2019-09-15 16:48:54 +0100

    Init Admin dashboard layout

---
+++ nuxtjs/layouts/admin.vue
@@ -0,0 +1,205 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        left
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Admin panel
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+          dense
+          nav
+      >
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            :to="item.to"
+            link
+            nuxt
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="$t('navigation.' + item.title)"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+        app
+        color="white"
+        light
+        tile
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Admin dashboard</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-menu
+          left
+          bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+              v-for="lang in 2"
+              :key="lang"
+              @click="() => {}"
+          >
+            <v-list-item-title>Lang {{ lang }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+    </v-app-bar>
+
+
+
+    <!--<v-navigation-drawer
+      :mini-variant.sync="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+      width="150"
+      hide-overlay
+    >
+
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn
+                icon
+                @click.stop="drawer = !drawer"
+            >
+              <v-icon v-html="'close'"></v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn
+              icon
+              @click.stop="miniVariant = !miniVariant"
+            >
+              <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+
+      <v-list>
+        <v-divider></v-divider>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="$t('navigation.' + item.title)"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-menu :nudge-width="100">
+        <v-toolbar-title slot="activator">
+          <span>{{ selectedLang.name }}</span>
+          <v-icon light>arrow_drop_down</v-icon>
+        </v-toolbar-title>
+        <v-list>
+          <v-list-item v-for="locale in availableLocales"
+                       :key="locale.code"
+                       @click="switchLocalePath(locale.code)">
+            <v-list-item-title v-text="locale.name"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer height="auto" class="grey darken-3">
+      <v-layout row wrap justify-center>
+        <v-btn
+            color="white"
+            text
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+        >
+          {{ $t('navigation.' + item.title) }}
+        </v-btn>
+        <v-flex xs12 py-3 text-xs-center white&#45;&#45;text>
+          &copy; {{ new Date().getFullYear() }} — <strong>apu314.com</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>-->
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false, // Nav Drawer default: closed.
+        left: true,
+
+        clipped: false,
+        fixed: false,
+        miniVariant: false,
+        items: [
+          { icon: 'home', title: 'home', to: '/' },
+          { icon: 'home', title: 'admin', to: '/admin' },
+          { icon: 'work', title: 'portfolio', to: '/portfolio' }
+          // { icon: 'content_paste', title: 'cv', to: '/cv' },
+          // { icon: 'build', title: 'services', to: '/services' },
+          // { icon: 'person', title: 'contact', to: '/contact' }
+        ],
+        languages: [
+          { title: 'Español', shortTitle: 'es' },
+          { title: 'English', shortTitle: 'en' }
+        ],
+        selectedLang: this.$i18n.locale,
+        title: 'Admin dashboard- apu314'
+      }
+    },
+    computed: {
+      availableLocales () {
+        return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+      }
+    }
+  }
+</script>
---
+++ nuxtjs/pages/admin.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+  import LangSwitcher from '../components/LangSwitcher'
+
+  export default {
+    name: 'admin',
+    data () {
+      return {
+        //title: this.$t('index.title')
+        title: 'Admin dashboard - apu314'
+      }
+    },
+    head () {
+      return {
+        // title: this.$t('index.title'),
+        title: 'Admin dashboard - apu314',
+        meta: [
+          { name: 'theme-color', content: '#20c0b1' },
+          { property: 'og:title', content: this.$t('index.title') },
+          { property: 'og:description', content: this.$t('index.subtitle') },
+          { hid: 'description', name: 'description', content: this.$t('index.subtitle') },
+          { property: 'og:url', content: 'https://apu314.com/' }
+        ]
+      }
+    },
+    components: {
+      LangSwitcher
+    },
+    layout: 'admin'
+  }
+</script>
+
